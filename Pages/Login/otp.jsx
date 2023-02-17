8126084c63f1af3c8920ce264b79e5ae404b4387
SelnoxInfotech <117904460+SelnoxInfotech@users.noreply.github.com>
2023-02-17 11:36:55 +0530

    Delete Pages directory

--- Pages/Login/otp.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function otp() {
-  return (
-    <div>otp</div>
-  )
-}
--- Pages/Pagenotfound.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Pagenotfound() {
-  return (
-    <div>Pagenotfound</div>
-  )
-}
