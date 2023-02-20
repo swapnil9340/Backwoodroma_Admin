13285a0b3e656cdba8e5099d0909d1e109edef3c
Selnoxinfotech <selnox93@gmail.com>
2023-02-20 11:37:40 +0530

    commeit

--- src/Pages/Product/ProductComponent/ProductCategory.jsx
+++ src/Pages/Product/ProductComponent/ProductCategory.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Select, MenuItem } from '@mui/material';
+// import { Select, MenuItem } from '@mui/material';
 import axios from "axios"
 import Cookies from 'universal-cookie';
-import { CheckBox } from '@mui/icons-material';
+// import { CheckBox } from '@mui/icons-material';
 
 export default function ProductCategory({ Product, SetProduct }) {
 
     const [Category, SetCategory] = React.useState([])
-    const [SubCategory, SetSubCategory] = React.useState(0)
+    const [selectedValue, setSelectedValue] = React.useState(0);
+    const handleCheckboxChange = (event) => {
+        setSelectedValue(parseInt(event.target.value));
+    };
+
+
 
     const cookies = new Cookies();
     const token_data = cookies.get('Token_access')
@@ -26,61 +31,60 @@ export default function ProductCategory({ Product, SetProduct }) {
             // SetProduct(Product => ({ ...Product, Sub_Category_id: response.data[0].id }))
         })
     }, [SetProduct, token_data, Product.Sub_Category_id])
-    
-    const formcontrolcheckbox = [...document.querySelectorAll(".form-control-checkbox")]
-    formcontrolcheckbox.forEach((onebyone) => {
-        
-        
-        onebyone.addEventListener("click", function () {
-            formcontrolcheckbox.forEach((onebyone) => {
-                onebyone.checked = false
-            })
-            onebyone.checked = true
-            SetSubCategory({[onebyone.id]: true});
-        })
-    })
-    
 
-// const handleChange = (event) => {
-//     const value = event.target.value;
-//     SetProduct({
-//         ...Product, [event.target.name]: value
-//     });
-// };
 
-return (
-    <div>
-        <div className='col background'>
-            <div className='col-10  center '>
-                <label className=' '>
-                    Category
-                </label>
-                <div className='col justify  Add_Category center'>
-                    <div className='col' >
-                        <div className="Product_category">
-                           
-                            {
-                                Category.map((data) => {
-                                    return (
-                                        <>
-                                            <div className='form-group-checkboxdev'>
-                                                <input type="checkbox" id={data.id} value={data.id}  className="form-control-checkbox" />
-                                                <span> {data.name} </span>
+
+
+
+    // const handleChange = (event) => {
+    //     const value = event.target.value;
+    //     SetProduct({
+    //         ...Product, [event.target.name]: value
+    //     });
+    // };
+
+    return (
+        <div>
+            <div className='col background'>
+                <div className='col-10  center '>
+                    <label className=' '>
+                        Category
+                    </label>
+                    <div className='col justify  Add_Category center'>
+                        <div className='col' >
+                            <div className="Product_category">
+
+                                {
+                                    Category.map((data, index) => {
+                                        return (
+                                            <div key={index}>
+
+                                                <div className='form-group-checkboxdev' key={index.id} >
+
+                                                    <input
+                                                        type="checkbox"
+                                                        value={data.id}
+                                                        checked={selectedValue === parseInt(data.id)}
+                                                        onChange={handleCheckboxChange}
+                                                    />
+
+                                                    <span> {data.name} </span>
+
+                                                </div>
+                                                {
+                                                    selectedValue === data.id ? "ghh" : null
+                                                }
 
                                             </div>
-                                            {
-                                                SubCategory[data.id] ?"ghh":null
-                                            }
-                                        </>
-                                    )
-                                })
-                          }
+                                        )
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
