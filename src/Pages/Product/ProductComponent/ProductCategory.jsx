cc768a96cda47939158ff8a33dd291e602fee766
swapnil9340 <95558311+swapnil9340@users.noreply.github.com>
2023-02-20 07:58:39 +0530

    kk

--- src/Pages/Product/ProductComponent/ProductCategory.jsx
+++ src/Pages/Product/ProductComponent/ProductCategory.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Select, MenuItem } from '@mui/material';
 import axios from "axios"
 import Cookies from 'universal-cookie';
+import { CheckBox } from '@mui/icons-material';
 
 export default function ProductCategory({ Product, SetProduct }) {
 
     const [Category, SetCategory] = React.useState([])
+    const [SubCategory, SetSubCategory] = React.useState(0)
 
     const cookies = new Cookies();
     const token_data = cookies.get('Token_access')
@@ -24,83 +26,78 @@ export default function ProductCategory({ Product, SetProduct }) {
             // SetProduct(Product => ({ ...Product, Sub_Category_id: response.data[0].id }))
         })
     }, [SetProduct, token_data, Product.Sub_Category_id])
+    
+    const formcontrolcheckbox = [...document.querySelectorAll(".form-control-checkbox")]
+    formcontrolcheckbox.forEach((onebyone) => {
+        
+        
+        onebyone.addEventListener("click", function () {
+            formcontrolcheckbox.forEach((onebyone) => {
+                
+                onebyone.checked = false
+                // SetSubCategory(false);
+                // SetSubCategory({
+                //     ...SubCategory,
+                //     [onebyone.id]: false
+                // });
+                
+            })
+            onebyone.checked = true
+            // SetSubCategory(onebyone.value)
+            // SetSubCategory(true)
+              
+              SetSubCategory(false);
+            SetSubCategory({
+                ...SubCategory,
+                [onebyone.id]: true
+            });
 
+            
+        })
+    })
+    console.log(SubCategory)
+    
 
-    function myFunction(e) {
-        console.log(e.target.value)
-        // var checkBox = document.getElementById("myCheck");
-        // var text = document.getElementById("text");
-        // if (checkBox.checked == true){
-        //   text.style.display = "block";
-        // } else {
-        //    text.style.display = "none";
-        // }
-    }
-    var show = true;
-  
-    function myFunction(data) {
-console.log(data)
-        var checkBoxes = 
-            document.getElementById(data);
+// const handleChange = (event) => {
+//     const value = event.target.value;
+//     SetProduct({
+//         ...Product, [event.target.name]: value
+//     });
+// };
 
-        if (show) {
-            checkBoxes.style.display = "block";
-            show = false;
-        } else {
-            checkBoxes.style.display = "none";
-            show = true;
-        }
- console.log(data)
-    }
+return (
+    <div>
+        <div className='col background'>
+            <div className='col-10  center '>
+                <label className=' '>
+                    Category
+                </label>
+                <div className='col justify  Add_Category center'>
+                    <div className='col' >
+                        <div className="Product_category">
+                           
+                            {
+                                Category.map((data) => {
+                                    return (
+                                        <>
+                                            <div className='form-group-checkboxdev'>
+                                                <input type="checkbox" id={data.id} value={data.id}  className="form-control-checkbox" />
+                                                <span> {data.name} </span>
 
-
-
-
-    const handleChange = (event) => {
-        const value = event.target.value;
-        SetProduct({
-            ...Product, [event.target.name]: value
-        });
-    };
-
-    return (
-        <div>
-            <div className='col background'>
-                <div className='col-10  center '>
-                    <label className=' '>
-                        Category
-                    </label>
-                    <div className='col justify  Add_Category center'>
-                        <div className='col' >
-                            <div className="Product_category">
-                                {
-                                    Category.map((data, index) => {
-                                        return (
-
-                                            <>
-                                              <div>
-                                                  <input value={data.id} type="checkbox" id="myCheck" onClick={myFunction(data.name)} key={data.id} />
-                                                <label htmlFor="myCheck">{data.name}</label>
-
-                                              </div>
-                                                <div id="JDF">
-                                                    <label for="first">
-                                                        <input type="checkbox" id="first" />
-                                                        checkBox1
-                                                    </label>
-
-                                                </div>
-                                                {/* <p id="text" style={{display:"none"}}>Checkbox is CHECKED!</p> */}
-
-                                            </>
-                                        )
-                                    })
-                                }
-                            </div>
+                                            </div>
+                                            {
+                                                SubCategory[data.id] ?"ghh":null
+                                            }
+                                        </>
+                                    )
+                                })
+                          }
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    </div>
+)
 }
+
