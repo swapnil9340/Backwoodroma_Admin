29af92f9b5024cf14b35952ebcd6b1d07e10bf19
Chandrajeet123 <selnoxreact@gmail.com>
2023-02-28 17:38:56 +0530

    change

--- src/Pages/Product/ProductComponent/Strain.jsx
+++ src/Pages/Product/ProductComponent/Strain.jsx
@@ -8,10 +8,10 @@ export default function Strain({ Product, SetProduct }) {
         s: "",
         c: "",
         h: "",
-})
-         React.useEffect(()=>{
-            SetStrain(Strain => ({ Strain, [Product.strain]: "active" }))
-         },[Product])
+    })
+    React.useEffect(() => {
+        SetStrain(Strain => ({ Strain, [Product.strain]: "active" }))
+    }, [Product])
 
 
     const handleChange = (event) => {
@@ -21,36 +21,43 @@ export default function Strain({ Product, SetProduct }) {
         });
         SetStrain(Strain => ({ Strain, [event.target.value]: "active" }))
     };
-        
-   
-    
+
+
+
     return (
+      
         <div>
-            <label className=' center'>
-                Strain
-            </label>
-            <div className='col-12   ' >
-                <div className='4'>
-                    <button name="strain" value={'N'} onClick={handleChange} className={`btn btton + ${Strain.N}` }> None</button>
-                </div>
-                <div className='4'>
-                    <button name="strain" value={'i'} onClick={handleChange} className={`btn btton + ${Strain.i}`} > Indica</button>
-                </div>
-                <div className='4'>
-                    <button name="strain" value={'s'} onClick={handleChange} className={`btn btton + ${Strain.s}`} > Sativa</button>
-                </div>
+        <label className=' center mt-4 ' style={{marginLeft:"10px"}}>
+              Strain
+          </label>
+  <div className="col-12 d-flex m-4">
 
-            </div>
-            <div className='col-12 Stack  ' >
-                <div className='4'>
-                    <button name="strain" value={'c'} onClick={handleChange} className={`btn btton + ${Strain.c}`} > CBD</button>
-                </div>
-                <div className='4'>
-                    <button name="strain" value={'h'} onClick={handleChange} className={`btn btton + ${Strain.h}`} > Hybrid</button>
-                </div>
+      <div className="col-4">
+         
+          <button name="strain" value={'N'} onClick={handleChange} className={` btton + ${Strain.N}`}> None</button>
 
-            </div>
-     </div>
- )
+      </div>
+    
+      <div className="col-4">
+      <button name="strain" value={'i'} onClick={handleChange} className={`btn btton + ${Strain.i}`} > Indica</button>
+      </div>
+      <div className="col-4">
+      <button name="strain" value={'s'} onClick={handleChange} className={`btn btton + ${Strain.s}`} > Sativa</button>
+      </div>
+
+  </div>
+
+ 
+  <div className='col-12 d-flex m-4  ' >
+      <div className='col-4'>
+          <button name="strain" value={'c'} onClick={handleChange} className={`btn btton + ${Strain.c}`} > CBD</button>
+      </div>
+      <div className='col-4'>
+          <button name="strain" value={'h'} onClick={handleChange} className={`btn btton + ${Strain.h}`} > Hybrid</button>
+      </div>
+
+  </div>
+</div>
+    )
 
 }
