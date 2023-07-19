e2ac90788256d5355fcf222f3475a60f57b2c7b0
Chandrajeet123 <selnoxreact@gmail.com>
2023-07-19 15:43:12 +0530

    signup

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -11,6 +11,8 @@ import useStyles from "../../Style";
 import { AiFillEye } from "react-icons/ai"
 import { InputAdornment } from "@mui/material";
 import Checkbox from '@mui/material/Checkbox';
+import { LoadingButton } from "@mui/lab";
+import Box from "@mui/material/Box";
 const Signup = () => {
     const classes = useStyles()
     const [Role, SetRole] = React.useState("")
@@ -20,144 +22,173 @@ const Signup = () => {
     return (
         <React.Fragment>
             <div className="container-fluid">
-                <div className="col-10 SignupContainer">
-                    <div className="col-12 SignupHeading_container">
-                        <div className="signupLogo_container">
-                            <LazyLoadImage className="signupLogo_image" src="./image/signup_logo.png" alt="image_not found" />
+                <div className="row">
+                    <div className="col-10 SignupContainer">
+                        <div className="col-12 SignupHeading_container">
+                            <div className="signupLogo_container">
+                                <LazyLoadImage className="signupLogo_image" src="./image/signup_logo.png" alt="image_not found" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-12 signupFields_container">
-                        <div className="col-10 signupFieldInner_Container">
-                            <form>
-                                <div className="col-12 signup_headings">
-                                    <h1 className="signup_headings">Signup</h1>
-                                </div>
-                                <div className="col-12 signup_user_main_container">
-                                    <div className="section  signup_image_mainContainer">
-                                        <div className="siginup_imgCont">
-                                            <LazyLoadImage className="signupUser_image" src="./image/blank_Image.webp" alt="image_not found" />
-
-                                        </div>
+                        <div className="col-12 signupFields_container">
+                            <div className="col-10  signupFieldInner_Container">
+                                <form>
+                                    <div className="col-12 signup_headings">
+                                        <h1 className="signup_headings">Signup</h1>
                                     </div>
-                                    <div className="section signupDropdown_rightSection">
-                                        <div className="col-12 roleDropDownContainer">
-                                            <p className="roleHead">Role</p>
-                                            <FormControl sx={{ minWidth: "30%" }}>
-                                                <Select
-                                                    id="RoleDropdown"
-                                                    value={Role}
-                                                    onChange={HandleRole}
-                                                >
+                                    <div className="col-12 signup_user_main_container">
+                                        <div className="section  signup_image_mainContainer">
+                                            <div className="siginup_imgCont">
+                                                <LazyLoadImage className="signupUser_image" src="./image/blank_Image.webp" alt="image_not found" />
 
-                                                    <MenuItem value={"Admin(Co Founder)"}>Admin(Co Founder)</MenuItem>
-
-                                                    <MenuItem value={"Content Manager"}>Content Manager</MenuItem>
-                                                    <MenuItem value={"Vendor Management"}>Vendor Management</MenuItem>
-                                                    <MenuItem value={"Store Management"}>Store Management</MenuItem>
-
-                                                </Select>
-                                            </FormControl>
-
-                                        </div>
-                                        <div className="col-12 signupNameFields">
-                                            <div>
-                                                <label htmlFor="name">Name</label>
                                             </div>
-                                            <TextField id="name" variant="outlined"
-                                                sx={{ minWidth: "30%" }}
-                                            />
+                                        </div>
+                                        <div className="section signupDropdown_rightSection">
+                                            <div className="col-12 roleDropDownContainer">
+                                                <div>
+                                                    <p className="roleHead">Role</p>
+
+                                                </div>
+                                                <FormControl
+                                                    className={classes.signuproleSelectDropdown}
+                                                >
+                                                    <Select
+                                                        size="small"
+                                                        id="RoleDropdown"
+                                                        value={Role}
+                                                        onChange={HandleRole}
+                                                    >
+
+                                                        <MenuItem value={"Admin(Co Founder)"}>Admin(Co Founder)</MenuItem>
+
+                                                        <MenuItem value={"Content Manager"}>Content Manager</MenuItem>
+                                                        <MenuItem value={"Vendor Management"}>Vendor Management</MenuItem>
+                                                        <MenuItem value={"Store Management"}>Store Management</MenuItem>
+
+                                                    </Select>
+                                                </FormControl>
+
+                                            </div>
+                                            <div className="col-12 signupNameFields">
+                                                <div>
+                                                    <label className="signup_name" htmlFor="name">Name</label>
+                                                </div>
+                                                <TextField id="name"
+                                                    variant="outlined"
+                                                    size="small"
+                                                    className={classes.signupNameTextFieldWidth}
+                                                />
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="col-12  signup_userNameCol">
+                                        <div>
+                                            <label className="userName" htmlFor="UserName">User Name</label>
+
+                                        </div>
+                                        <TextField variant="outlined"
+                                            className={classes.signupTextFieldWidth}
+                                            id="UserName"
+                                            size="small"
+                                            placeholder="User Name"
+                                        />
+                                    </div>
+                                    <div className="col-12 signupPhoneCol">
+                                        <div>
+                                            <label htmlFor="phoneNo">Phone</label>
+                                        </div>
+                                        <MuiPhoneNumber
+                                            className={classes.signupMuiPhone}
+                                            defaultCountry={'in'}
+                                            id="phoneNo"
+                                        />
+                                    </div>
+                                    <div className="col-12 userEmialFieldCol">
+                                        <div>
+                                            <label className="Useremail" htmlFor="Useremail">Useremail</label>
+                                        </div>
+                                        <TextField variant="outlined"
+                                            className={classes.signupTextFieldWidth}
+                                            id="Useremail"
+                                            size="small"
+                                            placeholder="User email"
+                                        />
+                                    </div>
+                                    <div className="col-12 passwordFiledCol ">
+                                        <div>
+                                            <label className="password" htmlFor="password">Password</label>
                                         </div>
 
-                                    </div>
-                                </div>
-                                <div className="col-12 mt-2">
-                                    <div>
-                                        <label htmlFor="UserName">User Name</label>
+                                        <TextField variant="outlined"
+                                            className={classes.signupTextFieldWidth}
+                                            id="password"
+                                            size="small"
+                                            placeholder="Password"
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <AiFillEye size={22} />
+                                                    </InputAdornment>
+                                                )
 
+                                            }}
+                                        />
                                     </div>
-                                    <TextField variant="outlined"
-                                        sx={{ minWidth: "50%" }}
-                                        id="UserName"
-                                        placeholder="User Name"
-                                    />
-                                </div>
-                                <div className="col-12 mt-4">
-                                    <MuiPhoneNumber
-                                        className={classes.signupMuiPhone}
-                                        defaultCountry={'in'}
-                                    />
-                                </div>
-                                <div className="col-12 userEmialFieldCol">
-                                    <div>
-                                        <label htmlFor="Useremail">Useremail</label>
+                                    <div className="col-12 passwordFiledCol ">
+                                        <div>
+                                            <label className="password" htmlFor="ConfirmPassword">Confirm Password</label>
+                                        </div>
+
+                                        <TextField variant="outlined"
+                                            size="small"
+                                            className={classes.signupTextFieldWidth}
+                                            id="ConfirmPassword"
+                                            placeholder="Confirm Password"
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <AiFillEye size={22} />
+                                                    </InputAdornment>
+                                                )
+
+                                            }}
+                                        />
                                     </div>
-                                    <TextField variant="outlined"
-                                        sx={{ minWidth: "50%", marginTop: "10px" }}
-                                        id="Useremail"
-                                        placeholder="User email"
-                                    />
-                                </div>
-                                <div className="col-12 passwordFiledCol ">
-                                    <div>
-                                        <label htmlFor="password">Password</label>
-                                    </div>
-
-                                    <TextField variant="outlined"
-                                        sx={{ minWidth: "50%", marginTop: "10px" }}
-                                        id="password"
-                                        placeholder="Password"
-                                        InputProps={{
-                                            endAdornment: (
-                                                <InputAdornment position="end">
-                                                    <AiFillEye size={22} />
-                                                </InputAdornment>
-                                            )
-
-                                        }}
-                                    />
-                                </div>
-                                <div className="col-12 passwordFiledCol ">
-                                    <div>
-                                        <label htmlFor="ConfirmPassword">Confirm Password</label>
-                                    </div>
-
-                                    <TextField variant="outlined"
-                                        sx={{ minWidth: "50%", marginTop: "10px" }}
-                                        id="ConfirmPassword"
-                                        placeholder="Confirm Password"
-                                        InputProps={{
-                                            endAdornment: (
-                                                <InputAdornment position="end">
-                                                    <AiFillEye size={22} />
-                                                </InputAdornment>
-                                            )
-
-                                        }}
-                                    />
-                                </div>
-                                <div className="col-12 ">
-                                    <p className="statusead">Status</p>
-                                    <FormControl sx={{ minWidth: "50%" }}>
-                                        <Select
-                                            id="RoleDropdown"
-                                            value={Role}
-                                            onChange={HandleRole}
+                                    <div className="col-12 ">
+                                        <p className="status">Status</p>
+                                        <FormControl
+                                            className={classes.signupStatusSelectDropdown}
                                         >
-                                            <MenuItem value={"Active"}>Active</MenuItem>
-                                            <MenuItem value={"Hide"}>Hide</MenuItem>
-                                        </Select>
-                                    </FormControl>
+                                            <Select
+                                                size="small"
+                                                id="RoleDropdown"
+                                                value={Role}
+                                                onChange={HandleRole}
+                                            >
+                                                <MenuItem value={"Active"}>Active</MenuItem>
+                                                <MenuItem value={"Hide"}>Hide</MenuItem>
+                                            </Select>
+                                        </FormControl>
 
-                                </div>
-                                <div className="col-12">
-                                <Checkbox  defaultChecked/>
-                                </div>
-                            </form>
+                                    </div>
+                                    <div className="col-12 signupConditionCol">
+                                        <Checkbox defaultChecked /><p className="signiningCondPara">By signing UP I agree with<span className="signTermCond">Term and conditions</span> </p>
+                                    </div>
+                                    <Box className={`signUp_loading_btn ${classes.SignuploadingBtnTextAndBack}`}>
+                                        <LoadingButton>Sign Up</LoadingButton>
+
+                                    </Box>
+                                    <div className="col-6 center alreadyAccount">
+                                        <p className="signiningCondPara">Already have an Account?<span className="signTermCond">Signin</span> </p>
+                                    </div>
+                                </form>
+                            </div>
+
                         </div>
 
                     </div>
-
                 </div>
+
 
             </div>
         </React.Fragment>
--- src/Style.jsx
+++ src/Style.jsx
@@ -3,9 +3,84 @@ const useStyles = makeStyles({
     signupMuiPhone: {
         "&.MuiTextField-root": {
           width:"50%",
+          marginTop:"15px"
 
         },
+        "@media(max-width:700px)":{
+          "&.MuiTextField-root":{
+            width:"100%",
+           
+          },
+        }
+    },
+    signupNameTextFieldWidth:{
+    "&.MuiTextField-root":{
+      width:"42%",
+     
+    },
+   
+    "@media(max-width:700px)":{
+      "&.MuiTextField-root":{
+        width:"100%",
        
+      },
     }
+    },
+    signupTextFieldWidth:{
+      "&.MuiTextField-root":{
+        width:"50%",
+      marginTop:"15px"
+      },
+      "@media(max-width:700px)":{
+        "&.MuiTextField-root":{
+          width:"100%",
+         
+        },
+      }
+      },
+    signuproleSelectDropdown:{
+      "&.MuiFormControl-root":{
+        width:"42%"
+      },
+      "@media(max-width:700px)":{
+        "&.MuiFormControl-root":{
+          width:"100%"
+        },
+      }
+ 
+
+    },
+    signupStatusSelectDropdown:{
+      "&.MuiFormControl-root":{
+        width:"50%"
+      },
+      "@media(max-width:700px)":{
+        "&.MuiFormControl-root":{
+          width:"100%"
+        },
+      }
+ 
+
+    },
+    SignuploadingBtnTextAndBack: {
+      "& .MuiButton-text": {
+        fontSize: ".5rem",
+      },
+      "& .MuiLoadingButton-root": {
+        fontSize:"14px",
+        width: "25%",
+        height: "38px",
+        borderRadius: "4px",
+        backgroundColor: "#ffff",
+        color: "#31B665",
+        textTransform: "none",
+        border: "2px solid #31B665"
+      },
+      "& .MuiButtonBase-root:hover": {
+        color: "#ffff",
+        backgroundColor: "#00b96a",
+        border: "2px solid #31B665"
+      },
+    },
 })
 export default useStyles
