fe4d79b29347a22bc055eec535f39b2835898442
swapnil chauhan <selnox93@gmail.com>
2023-08-21 15:50:02 +0530

    gkljdlsk

--- src/Pages/Login/Login_logout.jsx
+++ src/Pages/Login/Login_logout.jsx
@@ -119,7 +119,6 @@ export default function Login_logout() {
         <>
 
             <div className='container  center '>
-
                 <div className='row'>
                     <div className='col-12 login_logout_center'>
 
@@ -223,94 +222,7 @@ export default function Login_logout() {
                                 </div>
                             </div>
                         </div>
-                        <div className='col-12 '>
-
-                            <div className='col-12  label top  con'>
-                                <div className='col-sm-5'>
-
-                                    <label className='label'>
-                                        <span className='required'>*</span>
-                                        Name:
-                                    </label>
-                                </div>
-                                <div className='col display'>
-                                    <TextField placeholder='User Name' id="outlined-basic" variant="outlined"
-                                        name="username" style={{ width: 286 }} inputProps={{ style: { fontSize: 15, height: 5 } }}
-                                        onChange={handleChange}
-                                        value={inputs.username || ""}
-                                        className={classes.Username}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className='col-12  label top  con'>
-                                <div className='col-sm-5'>
-
-                                    <label className='label'>
-                                        <span className='required'>*</span>
-                                        Email:
-                                    </label>
-                                </div>
-                                <div className='col display'>
-                                    <TextField placeholder='Email Address ' id="outlined-basic" variant="outlined" name="Email"
-                                        type="email" style={{ width: 286, fontSize: 15 }} inputProps={{ style: { fontSize: 15, height: 5 } }}
-                                        value={inputs.Email || ""}
-                                        onChange={handleChange}
-                                        className={classes.Username}
-                                    />
-                                </div>
-                            </div>
-                            <div className='col-12  label top  con'>
-                                <div className='col-sm-5'>
-
-                                    <label className='label'>
-                                        <span className='required'>*</span>
-                                        Password:
-                                    </label>
-                                </div>
-                                <div className='col  display '>
-                                    <TextField placeholder='Password' type={values.showPassword ? "text" : "password"} id="outlined-basic" variant="outlined"
-                                        name="password" style={{ minWidth: 190 }} inputProps={{ style: { fontSize: 15, height: 5 } }}
-                                        onChange={handleChange}
-                                        className={classes.Username}
-                                        InputProps={{
-                                            endAdornment: (
-                                                <InputAdornment position='end'>
-                                                    <IconButton
-                                                        aria-label="toggle password visibility"
-                                                        onClick={handleClickShowPassword}
-                                                    >
-                                                        {values.showPassword ? <AiFillEye size={20} color='#747474' /> : <AiFillEyeInvisible size={20} color='#747474' />}
-                                                    </IconButton>
-                                                </InputAdornment>
-
-                                            )
-                                        }}
-
-                                        value={inputs.password || ""}
-
-                                    />
-
-                                </div>
-                            </div>
-                            <div className='top'>
-                                <input type="checkbox" name='checkbox' value={inputs.checkbox || ""} onChange={handleChange} />
-                                <label className='RememberMeCheckBox'>
-                                    Remember me
-                                </label>
-                            </div>
-                            <div className={classes.SubmitLoginButton}>
-
-                                <LoadingButton loading={isLoggedIn} onClick={handleSubmit}> Submit </LoadingButton>
-
-
-                            </div>
-
-                            <div className='col-12   check'>
-
-                                <Link to={"/Forgot"}> <button className='color btn'>Forgot Password?</button></Link>
-                            </div>
-                        </div>
+                
 
                     </div>
                     <div>
@@ -352,9 +264,9 @@ export default function Login_logout() {
 
 
                 </div>
-            </div>
-        </>
+                </div>
+            </>
 
 
-    )
+            )
 }
--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -38,16 +38,16 @@ export default function News() {
         //     field: 'Image', headerName: 'Post Image', editable: false, headerClassName: 'super-app-theme--header',maxWidth: 150, minWidth: 80, flex: 1,
         //     renderCell: (params) => <img src={"http://backend.sweede.net/" + params.value} alt="Alt_Text" width="35" height="30" />,
         // },
-        { field: 'Title', headerName: 'Post Title', editable: false, headerClassName: 'super-app-theme--header',maxWidth: 150, minWidth: 80, flex: 1,},
+        { field: 'Title', headerName: 'Post Title', editable: false, headerClassName: 'super-app-theme--header', maxWidth: 150, minWidth: 80, flex: 1, },
 
         // { field: 'Link', headerName: 'Link', editable: false, headerClassName: 'super-app-theme--header',maxWidth: 150, minWidth: 80, flex: 1, },
         {
             field: 'created', headerName: 'Publish Date', editable: false, maxWidth: 150, minWidth: 80, flex: 1, headerClassName: 'super-app-theme--header',
             renderCell: (params) => params.row.created.slice(0, 10)
         },
-        { field: 'Status', headerName: 'Views', editable: false,maxWidth: 150, minWidth: 80, flex: 1, headerClassName: 'super-app-theme--header' },
+        { field: 'Status', headerName: 'Views', editable: false, maxWidth: 150, minWidth: 80, flex: 1, headerClassName: 'super-app-theme--header' },
         {
-            field: 'Edit', headerName: 'Edit', editable: false, maxWidth: 150, minWidth: 80, flex: 1,headerClassName: 'super-app-theme--header',
+            field: 'Edit', headerName: 'Edit', editable: false, maxWidth: 150, minWidth: 80, flex: 1, headerClassName: 'super-app-theme--header',
             renderCell: (params) => (
                 <>
                     <Box
@@ -64,19 +64,19 @@ export default function News() {
                         }}
 
                     >
-                        <Select 
-                        sx={{
-                            boxShadow: '', '.MuiOutlinedInput-notchedOutline': { border: "0px" },
-                            "&.Mui-focused .MuiSelect-icon": { color: "#31B665" },
-                            "&:hover": {
-                                ".MuiSelect-icon": {
-                                    color: "#31B665"
-                                }
-                            },
-                        }}
-                        IconComponent={BsThreeDotsVertical} labelId="demo-simple-select-error-label">
-                         <NewsEdit data={params.row}></NewsEdit>
-                         <DeleteNews data={params.row}></DeleteNews> 
+                        <Select
+                            sx={{
+                                boxShadow: '', '.MuiOutlinedInput-notchedOutline': { border: "0px" },
+                                "&.Mui-focused .MuiSelect-icon": { color: "#31B665" },
+                                "&:hover": {
+                                    ".MuiSelect-icon": {
+                                        color: "#31B665"
+                                    }
+                                },
+                            }}
+                            IconComponent={BsThreeDotsVertical} labelId="demo-simple-select-error-label">
+                            <NewsEdit data={params.row}></NewsEdit>
+                            <DeleteNews data={params.row}></DeleteNews>
                         </Select>
                     </Box>
                 </>
@@ -108,108 +108,108 @@ export default function News() {
         <>
             <div className='container-fluid'>
                 <div className='row'>
-                <div className='col-10  category_main_row' >
+                    <div className='col-10  category_main_row' >
 
-                    <div className='col-12 Add_Category margin_top  m-2 mt-5 mb-5'>
-                        <div className="col"> <h2>Latest News
-                        </h2></div>
-                        <div className="col cat_but popup_A" >  <span className='btn cat_pop_btn'> <h2> <Newspop></Newspop></h2></span></div>
-                    </div>
+                        <div className='col-12 Add_Category margin_top  m-2 mt-5 mb-5'>
+                            <div className="col"> <h2>Latest News
+                            </h2></div>
+                            <div className="col cat_but popup_A" >  <span className='btn cat_pop_btn'> <h2> <Newspop></Newspop></h2></span></div>
+                        </div>
 
 
-                
-                   
 
-                                <div className='col-12' >
-                                    <Box 
-                                    sx={{  
-                                        height: 400,
-                                        width: '100%',
+
+
+                        <div className='col-12' >
+                            <Box
+                                sx={{
+                                    height: 400,
+                                    width: '100%',
+                                    '& .MuiButton-root': {
+                                        color: '#FFFFFF',
+                                        display: "flex",
+                                    },
+                                    '& .MuiDataGrid-columnHeaders': {
+                                        backgroundColor: '#E1FFED',
+                                    },
+                                    "@media(max-width:767px)": {
                                         '& .MuiButton-root': {
-                                            color: '#FFFFFF',
-                                            display: "flex",
+                                            display: "contents",
+                                            width: "150px",
+                                            margin: "2px",
+                                            fontSize: "14px"
                                         },
-                                        '& .MuiDataGrid-columnHeaders': {
-                                            backgroundColor: '#E1FFED',
-                                        },
-                                        "@media(max-width:767px)": {
-                                            '& .MuiButton-root': {
-                                                display: "contents",
-                                                width: "150px",
-                                                margin: "2px",
-                                                fontSize: "14px"
-                                            },
-            
-                                        },
-                                        "@media(max-width:546px)": {
-                                            '& .MuiButton-root': {
-                                                display: "contents",
-                                                width: "150px",
-                                                fontSize: "9px"
-                                            },
-            
-                                        },
-            
-                                        "@media(min-width:768px)": {
-                                            '& .MuiButton-root': {
-                                                width: "110px",
-                                                margin: "2px",
-                                                fontSize: "14px"
-                                            },
-            
-                                            "&.MuiDataGrid-root .MuiDataGrid-columnHeaderDraggableContainer": {
-                                                width: "120px"
-                                            }
-                                        },
-                                        ".MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within": {
-                                            outline: "none"
-                                        },
-                                    
-                                    }}
-                                    >
-                                    <ThemeProvider theme={CustomFontTheme}>
-                                        <div style={{ height: 500, width: '100%', }}>
-                                            <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }} checkboxSelection
 
-                                                sx={{
-                                                    
+                                    },
+                                    "@media(max-width:546px)": {
+                                        '& .MuiButton-root': {
+                                            display: "contents",
+                                            width: "150px",
+                                            fontSize: "9px"
+                                        },
+
+                                    },
+
+                                    "@media(min-width:768px)": {
+                                        '& .MuiButton-root': {
+                                            width: "110px",
+                                            margin: "2px",
+                                            fontSize: "14px"
+                                        },
+
+                                        "&.MuiDataGrid-root .MuiDataGrid-columnHeaderDraggableContainer": {
+                                            width: "120px"
+                                        }
+                                    },
+                                    ".MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within": {
+                                        outline: "none"
+                                    },
+
+                                }}
+                            >
+                                <ThemeProvider theme={CustomFontTheme}>
+                                    <div style={{ height: 500, width: '100%', }}>
+                                        <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }} checkboxSelection
+
+                                            sx={{
+
+                                                ".MuiDataGrid-toolbarContainer": {
+                                                    backgroundColor: "#31B665"
+                                                },
+                                                "@media(max-width:768px)": {
                                                     ".MuiDataGrid-toolbarContainer": {
-                                                        backgroundColor: "#31B665"
-                                                    },
-                                                    "@media(max-width:768px)": {
-                                                        ".MuiDataGrid-toolbarContainer": {
-                                                            gap: "10px",
-        
-                                                        }
-                                                    },
-                                                    "@media(max-width:546px)": {
-                                                        ".MuiDataGrid-toolbarContainer": {
-                                                            gap: "5px",
-        
-                                                        }
-                                                    },
-                                                    "&.MuiDataGrid-root  .MuiDataGrid-columnHeader:focus": {
-                                                        outline: "none"
-                                                    },
-                                                    "&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
-                                                        outline: "none ",
-                
-                                                    },
-                                                    "&.MuiDataGrid-root .MuiDataGrid-columnSeparator": {
-                                                        visibility: "hidden"
-                                                    },
-                                                    "&.MuiDataGrid-root .MuiDataGrid-row:hover": {
-                                                        backgroundColor: "#FFFFFF"
-                                                    },
-                                                }}
+                                                        gap: "10px",
 
-                                            />
-                                        </div>
-                                    </ThemeProvider>
-                                    </Box>
-                                </div>
-            </div>
-            </div>
+                                                    }
+                                                },
+                                                "@media(max-width:546px)": {
+                                                    ".MuiDataGrid-toolbarContainer": {
+                                                        gap: "5px",
+
+                                                    }
+                                                },
+                                                "&.MuiDataGrid-root  .MuiDataGrid-columnHeader:focus": {
+                                                    outline: "none"
+                                                },
+                                                "&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
+                                                    outline: "none ",
+
+                                                },
+                                                "&.MuiDataGrid-root .MuiDataGrid-columnSeparator": {
+                                                    visibility: "hidden"
+                                                },
+                                                "&.MuiDataGrid-root .MuiDataGrid-row:hover": {
+                                                    backgroundColor: "#FFFFFF"
+                                                },
+                                            }}
+
+                                        />
+                                    </div>
+                                </ThemeProvider>
+                            </Box>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
