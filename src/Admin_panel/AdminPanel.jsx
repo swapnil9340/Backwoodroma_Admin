a6662e7089b318f8b965f1c138db734d17d71857
swapnil chauhan <selnox93@gmail.com>
2024-01-18 18:54:24 +0530

    chnagedashboard

--- src/Admin_panel/AdminPanel.jsx
+++ src/Admin_panel/AdminPanel.jsx
@@ -1,31 +1,23 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import DonetChartTotalsale from './DonetChartTotalsale'
 import RightPenalscore from './RightPenalscore'
 import StatusBarCard from './StatusBarCard'
 import TotalUserLineChart from './TotalUserLineChart'
 import Createcontext from '../Hooks/Context/Context'
 export default function AdminPanel() {
-  const { state  } = useContext(Createcontext)
-console.log(state ,'state')
+  const { state } = useContext(Createcontext)
   return (
-    <div className=''>
-      <div className='row'>
-        <div className=''></div>
-        <div className='col-sm-6 border top' >
-          <StatusBarCard></StatusBarCard>
-          <div className='row'>
-            <div className='col-sm-6 top admin_col'>
-              <TotalUserLineChart></TotalUserLineChart>
-            </div>
-            <div className='col-sm-6 top admin_col'>
-              <DonetChartTotalsale></DonetChartTotalsale>
-            </div>
-          </div>
-        </div>
-        <div className='col-sm-6 border top'>
-          <RightPenalscore></RightPenalscore>
-        </div>
+
+    <div className='row'>
+
+      <div className='col-12 ' >
+        <StatusBarCard></StatusBarCard>
+ 
       </div>
+      {/* <div className='col-sm-6  top'>
+        {/* <RightPenalscore></RightPenalscore> */}
+      {/* </div> */} 
     </div>
+
   )
 }
