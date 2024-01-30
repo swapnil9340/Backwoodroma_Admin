29fb95692affaa4e4b38156867a021f4cefaed28
swapnil chauhan <selnox93@gmail.com>
2024-01-30 19:15:01 +0530

    chnage

--- src/Admin_panel/Productstorelist.jsx
+++ src/Admin_panel/Productstorelist.jsx
@@ -1,14 +1,6 @@
-import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
-import Cookies from 'universal-cookie';
-const Productstorelist = ({ title  , Data1}) => {
-  const cookies = new Cookies();
-  const token_data = cookies.get('Token_access')
-   const [Data , SetData] =  React.useState([])
-
-
-    console.log (  Data1 !==  undefined && Data1)
+const Productstorelist = ({ title, Data1 }) => {
 
   return (
     <div className="list_container">
@@ -16,103 +8,36 @@ const Productstorelist = ({ title  , Data1}) => {
         <h3 className="listCardTitle">{title}</h3>
       </div>
       <ul className="dashboardList">
-           { Data1 !==  undefined &&
-            Data1.map((data)=>{
-            return(
+        {Data1 !== undefined &&
+          Data1.slice(0, 4).map((data) => {
+            return (
               <li className="">
-              <div className="dashboardListItem">
-                <div className="dashboardListItemImage">
-                  <div
-                    className="imageCircle"
-                  >
-                    <img src={data.Image} className="w-[100%] h-[1005]" />
+                <div className="dashboardListItem">
+                  <div className="dashboardListItemImage">
+                    <div
+                      className="imageCircle"
+                    >
+                      <img src={data.Image} className="w-[100%] h-[1005]" />
+                    </div>
+                  </div>
+                  <div className="dashboardListItemtext">
+                    <h3 className="list_title">{data?.ProductName || data?.StoreName}</h3>
+                    <div className="d-flex justify-content-between align-items-center">
+                      {data?.ProductSalesCount ?
+                        <>
+                          <span className="listunitSold">Unit {data?.ProductSalesCount}</span>
+                          <span className="listunitSold">${data?.Price || data?.SalesPrice}</span>
+                        </>
+                        :     
+                        <span className="listunitSold"> {data?.SalesPrice}</span>
+                     }
+                    </div>
                   </div>
                 </div>
-                <div className="dashboardListItemtext">
-                  <h3 className="list_title">{data.ProductName}</h3>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <span className="listunitSold">Unit {data.ProductSalesCount}</span>
-                    <span className="listunitSold">${data.Price}</span>
-                  </div>
-                </div>
-              </div>
-            </li>
+              </li>
             )
-            })
-           }
-        {/* <li className="">
-          <div className="dashboardListItem">
-            <div className="dashboardListItemImage">
-              <div
-                className="imageCircle"
-              >
-                <img src="./image/niceguy.png" className="w-[100%] h-[1005]" />
-              </div>
-            </div>
-            <div className="dashboardListItemtext">
-              <h3 className="list_title">YOLO GUMMIES: PEACH</h3>
-              <div className="d-flex justify-content-between align-items-center">
-                <span className="listunitSold">Unite 2000</span>
-                <span className="listunitSold">$10,000.0</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="dashboardListItem">
-            <div className="dashboardListItemImage">
-              <div
-                className="imageCircle"
-              >
-                <img src="./image/niceguy.png" className="w-[100%] h-[1005]" />
-              </div>
-            </div>
-            <div className="dashboardListItemtext">
-              <h3 className="list_title">YOLO GUMMIES: PEACH</h3>
-              <div className="d-flex justify-content-between align-items-center">
-                <span className="listunitSold">Unite 2000</span>
-                <span className="listunitSold">$10,000.0</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="dashboardListItem">
-            <div className="dashboardListItemImage">
-              <div
-                className="imageCircle"
-              >
-                <img src="./image/niceguy.png" className="w-[100%] h-[1005]" />
-              </div>
-            </div>
-            <div className="dashboardListItemtext">
-              <h3 className="list_title">YOLO GUMMIES: PEACH</h3>
-              <div className="d-flex justify-content-between align-items-center">
-                <span className="listunitSold">Unite 2000</span>
-                <span className="listunitSold">$10,000.0</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="dashboardListItem">
-            <div className="dashboardListItemImage">
-              <div
-                className="imageCircle"
-              >
-                <img src="./image/niceguy.png" className="w-[100%] h-[1005]" />
-              </div>
-            </div>
-            <div className="dashboardListItemtext">
-              <h3 className="list_title">YOLO GUMMIES: PEACH</h3>
-              <div className="d-flex justify-content-between align-items-center">
-                <span className="listunitSold">Unite 2000</span>
-                <span className="listunitSold">$10,000.0</span>
-              </div>
-            </div>
-          </div>
-        </li> */}
-
+          })
+        }
       </ul>
       <div className="viewAllBtnList ">
         {" "}
